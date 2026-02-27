7d769b95b7a9ce6ded62f92a2cab300dd1e69251
Lakshay Bindal <lakshay.bindal_cs.aiml24@gla.ac.in>
2026-02-27 17:00:17 +0530

    Add loop function to detect objects using IR sensor LOW

--- src/main.cpp
+++ src/main.cpp
@@ -6,3 +6,12 @@ void setup() {
   pinMode(IR_PIN, INPUT);
   Serial.begin(9600);
 }
+
+void loop() {
+  int irValue = digitalRead(IR_PIN);
+
+  if (irValue == LOW) {
+    Serial.println("Object Detected");
+  }
+  delay(200);
+}
